bc9df2287c48f9494256ae0b21abdd1207f6b720
ahmetk <ahmetk@ahmetk-MacBook-Air.local>
2026-02-28 15:33:18 +0300

    Update .gitignore and remove secrets

--- infra/init-db.sh
+++ infra/init-db.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE auth_db;
+    CREATE DATABASE authdb;
     CREATE DATABASE user_db;
-    CREATE DATABASE course_db;
-    CREATE DATABASE order_db;
-    CREATE DATABASE payment_db;
-    CREATE DATABASE enrollment_db;
-    CREATE DATABASE notification_db;
+    CREATE DATABASE coursedb;
+    CREATE DATABASE orderdb;
+    CREATE DATABASE paymentdb;
+    CREATE DATABASE enrollmentdb;
+    CREATE DATABASE notificationdb;
 EOSQL
